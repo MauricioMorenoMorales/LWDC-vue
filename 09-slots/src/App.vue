5225ca33fdf64abb71750fb8bafda6724585f960
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-04 09:00:35 -0600

    Starting with dinamic components

--- 09-slots/src/App.vue
+++ 09-slots/src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 	<div>
 		<img alt="Vue logo" src="./assets/logo.png" />
 		<HelloWorld msg="Welcome to Your Vue.js App" />
@@ -27,13 +27,61 @@
 				</div>
 			</div>
 		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 my-4">
+					<hobbies>
+						<template v-slot:default="slotProps">
+							<h2>{{ slotProps.hobby }}</h2>
+						</template>
+					</hobbies>
+				</div>
+				<div class="col-md-4 my-4">
+					<hobbies #default="{ hobby: name, item = 1 }">
+						<h1>{{ name }} {{ item }}</h1>
+					</hobbies>
+				</div>
+			</div>
+		</div>
+		<div class="my-5 py-5"></div>
 	</div>
+</template> -->
+
+<template lang="pug">
+div
+	img(alt="Vue Logo" src="./assets/logo.png")
+	HelloWorld(msg="Welcome to your Vue.js App")
+	Button: em.fa.fa-caret-up &nbsp; User Profile
+	div.container
+		div.row
+			div.col-md-4
+				profile-card
+					template(#content)
+						div Content
+					template(#footer)
+						div Footer Message
+			div.col-md-4
+				profile-card
+					template(v-slot:header)
+						div Header Title
+					template(#footer)
+						div Footer Message
+	div.container
+		div.row
+			div.col-md-4.my-4
+				hobbies
+					template(v-slot:default="slotProps")
+						h2 {{slotProps.hobby}}
+			div.col-md-4.my-4
+				hobbies(#default="{hobby: name, item = 1}")
+					h1 {{name}} {{item}}
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
 import Button from './components/Button.vue';
 import ProfileCard from './components/ProfileCard.vue';
+import Hobbies from './components/Hobbies.vue';
 
 export default {
 	name: 'App',
@@ -41,6 +89,7 @@ export default {
 		HelloWorld,
 		Button,
 		ProfileCard,
+		Hobbies,
 	},
 };
 </script>
---
+++ 09-slots/src/components/Hobbies.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+div
+	ul.list-group
+		li.list-group-item(v-for="hobby in hobbies" :key="hobby")
+			slot(:hobby="hobby")
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			hobbies: ['Reading', 'Writing', 'Learning'],
+			item: [1, 2, 3],
+		};
+	},
+};
+</script>
