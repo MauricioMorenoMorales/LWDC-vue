c658aa9b2b84f27aca1965a8c30a889910526874
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-06 10:47:36 -0600

    class 52

--- 12-router/src/views/CreatePost.vue
+++ 12-router/src/views/CreatePost.vue
@@ -1,7 +1,38 @@
 <template lang="pug">
-div Create Post Page
+div
+	h3 Create Post
+	form(@submit.prevent="onCreatePost()")
+		.form-group
+			label Title
+			input.form-control(type="text", v-model="title")
+		.form-group
+			label Description
+			textarea.form-control(type="text", v-model="description")
+		div
+			button.btn.btn-primary.my-4(type="submit") Create Post
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+	data() {
+		return {
+			title: '',
+			description: '',
+		};
+	},
+	methods: {
+		onCreatePost() {
+			axios
+				.post(`https://vue-completecourse.firebaseio.com/posts.json`, {
+					title: this.title,
+					description: this.description,
+				})
+				.then(response => {
+					console.log(response);
+					this.$router.push('/posts');
+				});
+		},
+	},
+};
 </script>
--- 12-router/src/views/Posts.vue
+++ 12-router/src/views/Posts.vue
@@ -1,7 +1,40 @@
 <template lang="pug">
-div Post Page
+div
+	h3 Post Details
+	table.table
+		thead
+			tr
+				th Id
+				th Title
+				th Description
+		tbody
+			tr(v-for="post in postDetails" :key="post[id]")
+				td {{post.id}}
+				td {{post.title}}
+				td {{post.description}}
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+	data() {
+		return {
+			postDetails: [],
+		};
+	},
+	mounted() {
+		axios
+			.get(`https://vue-completecourse.firebaseio.com/posts.json`)
+			.then(response => {
+				this.formatPosts(response.data);
+			});
+	},
+	methods: {
+		formatPosts(postsData) {
+			for (const key in postsData) {
+				this.postDetails.push({ ...postsData[key], id: key });
+			}
+		},
+	},
+};
 </script>
