b566b913bc5ceadb894e02103e7298469075143a
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-12-16 21:40:02 -0600

    clase 70

--- 13-animations/src/components/ListData.vue
+++ 13-animations/src/components/ListData.vue
@@ -1,12 +1,37 @@
 <template lang="pug">
 div
 	div
-		span.mx-1(v-for="number in numbers" :key="number") {{number}}
-	div
 		button(@click.prevent="onAdd()") Add
 		button(@click.prevent="onRemove()") Remove
 		button(@click.prevent="onShuffle()") Shuffle
+	div
+		transition-group(tag="div" name="list")
+			span.mx-1.list-item(v-for="number in numbers" :key="number") {{number}}
 </template>
+
+<style lang="stylus" scoped>
+.list-item
+	display inline-block
+.list-enter-from
+	opacity 0
+	transform translateY(30px)
+.list-enter-to
+	transform translateY(0)
+	opacity 1
+.list-enter-active
+	transition all 1s ease
+.list-leave-from
+	opacity 1
+	transform translateY(0)
+.list-leave-to
+	transform translateY(30px)
+	opacity 0
+.list-leave-active
+	transition all 1s ease
+	position absolute
+.list-move
+	transition all 1s ease
+</style>
 
 <script>
 export default {
